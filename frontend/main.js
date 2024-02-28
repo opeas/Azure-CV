9210432acbabffe5ccb67cf34e6ef6a9e41821ab
Oliwier Peas <oliwierpeas@gmail.com>
2024-02-28 12:17:23 +0100

    Stworzono storage i funkcje

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetCVCounter';
+const functionApiUrl = 'https://getazurecvcounter.azurewebsites.net/api/GetCVCounter?code=M9pJwpqUgqnf3lxAQlBmo4zP_GBdMGeDLL9LSDgr0NXaAzFuRGaMoQ==';
+const localFunctionApi = 'http://localhost:7071/api/GetCVCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
